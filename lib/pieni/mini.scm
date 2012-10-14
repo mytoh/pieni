d1ccea5ad990fbcafa5aed5a9e4057d15a82902a
mytoh <mytoh@gmail.com>
2012-10-14 23:13:43 +0900

    update

--- lib/pieni/mini.scm
+++ lib/pieni/mini.scm
@@ -4,7 +4,8 @@
 (define-module pieni.mini
   (export
     define-test
-    is)
+    is
+    ==)
   (use gauche.test))
 (select-module pieni.mini)
 
@@ -15,8 +16,7 @@
      (do-test
        (test-section (symbol->string 'name))
        expr
-       ...)))
-  )
+       ...))))
 
 
 (define-syntax is
@@ -24,11 +24,20 @@
     ((_ (checker expect form))
      (test (quote form) expect (lambda () form) checker))))
 
+(define (== t1 t2)
+  (cond
+    ((string? t1)
+     (string=? t1 t2))
+    ((number? t1)
+     (= t1 t2))
+    (else
+      (equal? t1 t2))
+    ))
+
 
 (define-syntax do-test
   (syntax-rules ()
     ((_ body ...)
      (begin
-     body
-     ...
-     ))))
+       body
+       ...))))
